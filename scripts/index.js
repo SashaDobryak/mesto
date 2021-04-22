cc152ca4e3edbd6c158771d562b11f21cabcdc8d
Sasha Dobryak <sasha@dobryak.com.ua>
2021-04-22 22:44:31 +0300

    проект 4: для ревью

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__batton-close');
+let popup = document.querySelector('.popup');
+let nameInput = document.querySelector('#popup__name');
+let jobInput = document.querySelector('#popup__job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form');
+
+function openPopup() { 
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function closePopup() { 
+    popup.classList.remove('popup_opened');
+}
+
+
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault(); 
+  profileName.textContent = nameInput.value
+  profileJob.textContent = jobInput.value
+  closePopup()
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
